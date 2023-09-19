2b26aa370f9c125864e3907a99aa7ac94b5d8dfd
Lauris Mierkalns <latpages@gmail.com>
2023-09-20 01:36:42 +0300

    Init

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace d3yii2\d3icon;
+
+/**
+ * d3icon module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'd3yii2\d3icon\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
